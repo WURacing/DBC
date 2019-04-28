1cf306b539eb528ca3114cc1680a37e2d1318558
Connor Monahan <cmonahan@wustl.edu>
2019-04-27 20:48:11 -0500

    Add dummy analog signals message
    Sorry <3

--- convert.sh
+++ convert.sh
@@ -12,5 +12,5 @@ export TEMPLATES=../templates.dbc2c
 for i in *.dbc; do
 	echo "#define ubyte uint8_t" > "${i%.dbc}".h
 	echo "#define uword uint16_t" >> "${i%.dbc}".h
-	cat "$i" | iconv -f CP1252 | mawk -f ../dbc2c.awk >> "${i%.dbc}".h
+	cat "$i" | iconv -f CP1252 | awk -f ../dbc2c.awk >> "${i%.dbc}".h
 done
